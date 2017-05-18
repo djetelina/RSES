ca2abc6c267361e87d22d23d244f4597855d852c
David Jetelina <djt@seznam.cz>
2017-05-19 00:36:50 +0200

    Pagination to do, added support for submoduling this repo

--- rses/src/config.py
+++ rses/src/config.py
@@ -4,7 +4,7 @@ import os
 
 SECRET_KEY: str = os.environ.get('SECRET_KEY', 'SUPER_SECRET')
 PORT: int = int(os.environ.get('PORT', 5000))
-DATABASE_URL: str = os.environ.get('DATABASE_URL')
+DATABASE_URL: str = os.environ.get ('RSES_DB_URL') or os.environ.get('DATABASE_URL')
 # Do you want a flask client
 RSES_WEB_CLIENT: bool = True
 # The master password to enter
