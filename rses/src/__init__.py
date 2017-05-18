47d68c4089857b545e1a881a217816718ac9df75
David Jetelina <djt@seznam.cz>
2017-05-19 00:53:22 +0200

    Fix for submoduling

--- rses/src/__init__.py
+++ rses/src/__init__.py
@@ -1,2 +1,9 @@
 # coding=utf-8
 """The python source"""
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from flask_app.blueprints.api.api import rses_api_bp
+from flask_app.blueprints.client.client import rses_web_client_bp
